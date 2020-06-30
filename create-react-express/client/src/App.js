1a397a070c37f316ec71d2d5f1f95087a096a467
David Ghazi <davidaghazi@gmail.com>
2020-06-30 19:41:29 -0400

    created WarGame file + layout.component.js

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import WarGame from './components/WarGame';
+import { Header, Layout } from './components/layout.component';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+  <div className = "App">
+    <Header>War Game</Header>
+    <Layout>
+      <WarGame />
+    </Layout>
+  </div>
+)};
 
 export default App;
