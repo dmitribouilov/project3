bf76eab64b9c1e55207e08fde711f5f1aca0d590
David Ghazi <davidaghazi@gmail.com>
2020-07-01 00:38:30 -0400

    added dealer element to war game

--- create-react-express/client/src/components/WarGame.js
+++ create-react-express/client/src/components/WarGame.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+//Dealer cards
+let dealer_deck = [];
+//Dealer current card drawn
+let dealer_card = [];
+
 //User 1 cards
 let user1_deck = [];
 //User 1 current card drawn
@@ -26,6 +31,8 @@ function WarGame() {
 
   function newGame(){
 
+    dealer_deck = [];
+    dealer_card = [];
     user1_deck = [];
     user1_card = [];
     user2_deck = [];
@@ -41,6 +48,7 @@ function WarGame() {
         currentDeck = result.data.deck_id;
 
         //Clear decks in both users hands
+        dealer_deck = [];
         user1_deck = [];
         user2_deck = [];
 
@@ -72,8 +80,19 @@ function WarGame() {
                 imageURL: `${card2.image}`
               };
 
-              user1_deck.push(drawnCard1);
-              user2_deck.push(drawnCard2);
+              let card3 = result.data.cards[i + 2];
+              //Card object to display user 2's hand
+              let drawnCard3 = {
+                value: `${card3.value}`,
+                suit: `${card3.suit}`,
+                imageURL: `${card3.image}`,
+                cardCode: `${card3.code}`,
+                imageURL: `${card3.image}`
+              };
+
+              dealer_deck.push(drawnCard1)
+              user1_deck.push(drawnCard2);
+              user2_deck.push(drawnCard3);
             }
             //Pushes new card to the browser
             updateRender(n => !n);
@@ -86,12 +105,15 @@ function WarGame() {
   function deal() {
     try {
       //reset the pile for each player
+      dealer_card = [];
       user1_card = [];
       user2_card = [];
       if (!gameWon()) {
+        let dealer = dealer_deck.pop()
         let player1 = user1_deck.pop();
         let player2 = user2_deck.pop();
-        if (!(player1 === "undefined" && player2 === "undefined")) {
+        if (!(dealer === "undefined" && player1 === "undefined" && player2 === "undefined")) {
+          dealer_card.push(dealer);
           user1_card.push(player1);
           user2_card.push(player2);
           updateRender(n => !n);
@@ -106,20 +128,31 @@ function WarGame() {
 
   //Scores for whoever wins round
   async function drawWinner() {
+
+    //get dealer card value 
+    let dealerScore = cardValue(dealer_card[dealer_card.length - 1]);
     //get player card value
     let user1Score = cardValue(user1_card[user1_card.length - 1]);
-    //get deal card value
+    //get player card value
     let user2Score = cardValue(user2_card[user2_card.length - 1]);
 
-    if (user1Score === user2Score) {
+    if (dealerScore === user1Score) {
       //its a tie
-      console.log('Tie')
-    } else if (user1Score > user2Score) {
+      console.log('Tie!')
+    }else if (dealerScore === user2Score){
+      //it's a tie
+      console.log('Tie!')
+    }else if (dealerScore > user1Score && dealerScore > user2Score) {
       //player wins the flip
-      console.log("Player 1 wins this round");
-    } else {
+      console.log("Both Players Drink!");
+    } else if (user1Score > dealerScore && dealerScore > user2Score) {
       //dealer wins
-      console.log("Player 2 wins this round");
+      console.log("Player 2 DRINK!");
+    }else if (user2Score > dealerScore && dealerScore > user1Score) {
+      //dealer wins
+      console.log("Player 1 DRINK!");
+    }else {
+      console.log("Nobody Drinks!")
     }
   }
 
@@ -155,7 +188,6 @@ function WarGame() {
 
   return (
     <>
-    <div>
       <div>
         <div>
           <button onClick={deal}>Hit</button>
@@ -164,6 +196,20 @@ function WarGame() {
           </button>
         </div>
       </div>
+      <div>
+        <div type="player">
+          <h2>Dealer</h2>
+          {dealer_card &&
+            dealer_card.map((card, index) => {
+              return (
+                <img
+                  key={`dealer${index}`}
+                  src={`${card.imageURL}`}
+                  alt={`${card.value} of ${card.suit}`}
+                />
+              );
+            })}
+        </div>
       <div>
         <div type="player">
           <h2>Player 1</h2>
@@ -184,7 +230,7 @@ function WarGame() {
             user2_card.map((card, index) => {
               return (
                 <img
-                  key={`dealer${index}`}
+                  key={`player${index}`}
                   src={`${card.imageURL}`}
                   alt={`${card.value} of ${card.suit}`}
                 />
@@ -193,9 +239,220 @@ function WarGame() {
         </div>
       </div>
     </div>
-  </>
+    </>
   )
 
 }
 
 export default WarGame;
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// //User 1 cards
+// let user1_deck = [];
+// //User 1 current card drawn
+// let user1_card = [];
+
+// //User 2 cards
+// let user2_deck = [];
+// //User 2 current card drawn
+// let user2_card =[];
+
+// function WarGame() {
+
+//   const [shuffled, setShuffled] = useState("");
+//   // const shuffled = useState("");
+//   const [,updateRender] = useState();
+//   let currentDeck;
+//   void shuffled;
+
+//   //Calls new game when the page loads
+//   useEffect(() => {
+//     newGame();
+//   }, []);
+
+//   function newGame(){
+
+//     user1_deck = [];
+//     user1_card = [];
+//     user2_deck = [];
+//     user2_card = [];
+//     //Make API call to retrieve cards for game
+//     axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
+//       .then(result => {
+//         result.data.success && console.log("Deck obtained");
+
+//         //Shuffle deck
+//         setShuffled(result.data.shuffled);
+//         //Retrieve deck
+//         currentDeck = result.data.deck_id;
+
+//         //Clear decks in both users hands
+//         user1_deck = [];
+//         user2_deck = [];
+
+//         //Make API request to evenly split cards between both users
+//         axios.get(`https://deckofcardsapi.com/api/deck/${currentDeck}/draw/?count=52`)
+//           .then(result => {
+//             result.data.success && console.log("Deck is split!")
+
+//             let i;
+//             for (i = 0; i < 52; i += 2){
+//               let card1 = result.data.cards[i];
+              
+//               //Card object to display user 1's hand
+//               let drawnCard1 = {
+//                 value: `${card1.value}`,
+//                 suit: `${card1.suit}`,
+//                 imageURL: `${card1.image}`,
+//                 cardCode: `${card1.code}`,
+//                 imageURL: `${card1.image}`
+//               };
+
+//               let card2 = result.data.cards[i + 1];
+//               //Card object to display user 2's hand
+//               let drawnCard2 = {
+//                 value: `${card2.value}`,
+//                 suit: `${card2.suit}`,
+//                 imageURL: `${card2.image}`,
+//                 cardCode: `${card2.code}`,
+//                 imageURL: `${card2.image}`
+//               };
+
+//               user1_deck.push(drawnCard1);
+//               user2_deck.push(drawnCard2);
+//             }
+//             //Pushes new card to the browser
+//             updateRender(n => !n);
+//           })
+//           .catch(error => console.log(error));
+//       })
+//     .catch(error => console.log(error));
+//   }
+
+//   function deal() {
+//     try {
+//       //reset the pile for each player
+//       user1_card = [];
+//       user2_card = [];
+//       if (!gameWon()) {
+//         let player1 = user1_deck.pop();
+//         let player2 = user2_deck.pop();
+//         if (!(player1 === "undefined" && player2 === "undefined")) {
+//           user1_card.push(player1);
+//           user2_card.push(player2);
+//           updateRender(n => !n);
+//           drawWinner();
+//         }
+//       } else {
+//         gameWon();
+//         updateRender(n => !n);
+//       }
+//     } catch (e) {}
+//   }
+
+//   //Scores for whoever wins round
+//   async function drawWinner() {
+//     //get player card value
+//     let user1Score = cardValue(user1_card[user1_card.length - 1]);
+//     //get deal card value
+//     let user2Score = cardValue(user2_card[user2_card.length - 1]);
+
+//     if (user1Score === user2Score) {
+//       //its a tie
+//       console.log('Tie')
+//     } else if (user1Score > user2Score) {
+//       //player wins the flip
+//       console.log("Player 1 wins this round");
+//     } else {
+//       //dealer wins
+//       console.log("Player 2 wins this round");
+//     }
+//   }
+
+//   //Create function to specify the value for A, K, Q, J
+//   function cardValue(card){
+//     try {
+//       switch (card.value) {
+//         case "ACE":
+//           return 14;
+//         case "KING":
+//           return 13;
+//         case "QUEEN":
+//           return 12;
+//         case "JACK":
+//           return 11;
+//         default:
+//           return card.value;
+//       }
+//     } catch (e) {}
+//   }
+
+//   //checks if either player has an empty deck
+//   function gameWon() {
+//     console.log("Player 1 Cards: " + user1_deck.length + " Player 2 Cards: " + user2_deck.length);
+//     if (user1_deck.length === 0) {
+//       return true;
+//     }
+//     if (user2_deck.length === 0) {
+//       return true;
+//     }
+//     return false;
+//   }
+
+//   return (
+//     <>
+//     <div>
+//       <div>
+//         <div>
+//           <button onClick={deal}>Hit</button>
+//           <button onClick={newGame}>
+//             New Game
+//           </button>
+//         </div>
+//       </div>
+//       <div>
+//         <div type="player">
+//           <h2>Player 1</h2>
+//           {user1_card &&
+//             user1_card.map((card, index) => {
+//               return (
+//                 <img
+//                   key={`player${index}`}
+//                   src={`${card.imageURL}`}
+//                   alt={`${card.value} of ${card.suit}`}
+//                 />
+//               );
+//             })}
+//         </div>
+//         <div>
+//           <h2>Player 2</h2>
+//           {user2_card &&
+//             user2_card.map((card, index) => {
+//               return (
+//                 <img
+//                   key={`dealer${index}`}
+//                   src={`${card.imageURL}`}
+//                   alt={`${card.value} of ${card.suit}`}
+//                 />
+//               );
+//             })}
+//         </div>
+//       </div>
+//     </div>
+//   </>
+//   )
+
+// }
+
+// export default WarGame;
