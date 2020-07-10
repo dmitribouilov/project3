da9e6a9c3cc429efb029abe00c7405971e761890
Anson Wong <ansonhywong@gmail.com>
2020-07-09 20:16:42 -0400

    update chat

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -3,6 +3,19 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//for chatapp
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+io.on("connection", socket => {
+  socket.emit("your id", socket.id);
+  socket.on("send message", body => {
+      io.emit("message", body)
+  })
+})
+//done
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
