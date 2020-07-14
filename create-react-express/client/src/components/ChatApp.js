d2a6b98a5cfb95457cefd1febd06a77f554b9f12
Anson Wong <ansonhywong@gmail.com>
2020-07-14 01:51:55 -0400

    fixing styling on chat & name

--- create-react-express/client/src/components/ChatApp.js
+++ create-react-express/client/src/components/ChatApp.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import io from "socket.io-client";
+import { Redirect } from "react-router-dom";
+// import List from "../list";
 
 const Page = styled.div`
   display: flex;
-  height: 100vh;
-  width: 100%;
-  align-items: center;
-  background-color: #46516e;
+  height: 50%;
+  width: 450px;
+  align-items: right;
+  background-color: #FFFFFF;
   flex-direction: column;
+  border: 2px solid black;
+  border-radius: 10px;
+  padding-bottom: 10px;
+  overflow: auto;
 `;
 
 const Container = styled.div`
@@ -18,10 +24,11 @@ const Container = styled.div`
   max-height: 500px;
   overflow: auto;
   width: 400px;
-  border: 1px solid lightgray;
+  border: 2px solid black;
   border-radius: 10px;
   padding-bottom: 10px;
   margin-top: 25px;
+  margin-left: 15px;
 `;
 
 const TextArea = styled.textarea`
@@ -33,24 +40,26 @@ const TextArea = styled.textarea`
   padding-top: 10px;
   font-size: 17px;
   background-color: transparent;
-  border: 1px solid lightgray;
+  border: 2px solid black;
   outline: none;
-  color: lightgray;
+  color: black;
   letter-spacing: 1px;
   line-height: 20px;
   ::placeholder {
-    color: lightgray;
+    color: black;
   }
+  margin-left: 15px;
 `;
 
 const Button = styled.button`
-  background-color: pink;
+  background-color: blue;
   width: 100%;
   border: none;
   height: 50px;
   border-radius: 10px;
-  color: #46516e;
+  color: white;
   font-size: 17px;
+  margin-left: 15px;
 `;
 
 const Form = styled.form`
@@ -66,8 +75,8 @@ const MyRow = styled.div`
 
 const MyMessage = styled.div`
   width: 45%;
-  background-color: pink;
-  color: #46516e;
+  background-color: blue;
+  color: white;
   padding: 10px;
   margin-right: 5px;
   text-align: center;
@@ -82,8 +91,8 @@ const PartnerRow = styled(MyRow)`
 const PartnerMessage = styled.div`
   width: 45%;
   background-color: transparent;
-  color: lightgray;
-  border: 1px solid lightgray;
+  color: lightblue;
+  border: 1px solid lightblue;
   padding: 10px;
   margin-left: 5px;
   text-align: center;
@@ -91,7 +100,7 @@ const PartnerMessage = styled.div`
   border-bottom-left-radius: 10%;
 `;
 
-const ChatApp = () => {
+const ChatApp = (props) => {
   const [yourID, setYourID] = useState();
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
@@ -115,11 +124,17 @@ const ChatApp = () => {
     setMessages(oldMsgs => [...oldMsgs, message]);
   }
 
-  function sendMessage(e) {
-    e.preventDefault();
+  function sendMessage(props) {
+    props.preventDefault();
+
+    // let me = props.location.state.me
+    // let opponent = props.location.state.opponent
+
     const messageObject = {
       body: message,
       id: yourID,
+      // userA = me,
+      // userB = opponent,
     };
     setMessage("");
     socketRef.current.emit("send message", messageObject);
@@ -128,6 +143,7 @@ const ChatApp = () => {
   function handleChange(e) {
     setMessage(e.target.value);
   }
+
 
   return (
     <Page>
@@ -138,6 +154,7 @@ const ChatApp = () => {
               <MyRow key={index}>
                 <MyMessage>
                   {message.body}
+                  {/* {userA.body} */}
                 </MyMessage>
               </MyRow>
             )
@@ -152,6 +169,7 @@ const ChatApp = () => {
         })}
       </Container>
       <Form onSubmit={sendMessage}>
+        {/* <TextArea value={name} onChange={handleChange} placeholder="Whats your displayname?" /> */}
         <TextArea value={message} onChange={handleChange} placeholder="Say something..." />
         <Button>Send</Button>
       </Form>
